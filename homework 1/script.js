1b910f26da2c66fa557047ff5f649aa93a0a0598
Ekaterina <karasenko.03@gmail.com>
2024-09-13 01:55:18 +0300

    first commit

---
+++ homework 1/script.js
@@ -0,0 +1,79 @@
+function translateToEnglish() {
+    document.getElementById('surname').innerText = 'Petrov';
+    document.getElementById('name').innerText = 'Alexandet';
+    document.getElementById('patronymic').innerText = 'Ivanovich';
+    document.getElementById('gender').innerText = 'male';
+    document.getElementById('birthplace').innerText = 'Saint-Peterburg';
+}
+
+document.getElementById('translateButton').addEventListener('click', translateToEnglish);
+
+function insertTable() {
+    let n = 5;
+    let table = document.querySelector('#table');
+    table.innerHTML = '';
+
+    for (let i = 0; i < n; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < n; j++) {
+            let td = document.createElement('td');
+            td.innerText = (i + 1) * (j + 1);
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+
+    let firstRow = table.querySelector('tr:first-child');
+    firstRow.querySelectorAll('td').forEach(cell => {
+        cell.style.backgroundColor = '#0288d1';
+        cell.style.color = 'white';
+    });
+
+    let allRows = table.querySelectorAll('tr');
+    allRows.forEach(row => {
+        let firstCell = row.querySelector('td:first-child');
+        firstCell.style.backgroundColor = '#0288d1';
+        firstCell.style.color = 'white';
+    });
+}
+
+function hideTable() {
+    let table = document.querySelector('#table');
+    table.innerHTML = '';
+}
+
+function toggleTable() {
+    let table = document.querySelector('#table');
+    let toggleButton = document.getElementById('toggleTableButton');
+
+    if (table.innerHTML === '') {
+        insertTable();
+        toggleButton.innerText = 'Скрыть таблицу';
+    } else {
+        hideTable();
+        toggleButton.innerText = 'Показать таблицу Пифагора';
+    }
+}
+
+document.getElementById('toggleTableButton').addEventListener('click', toggleTable);
+
+
+function hideTable() {
+    let table = document.querySelector('#table');
+    table.innerHTML = '';
+}
+
+function toggleTable() {
+    let table = document.querySelector('#table');
+    let toggleButton = document.getElementById('toggleTableButton');
+
+    if (table.innerHTML === '') {
+        insertTable();
+        toggleButton.innerText = 'Скрыть таблицу';
+    } else {
+        hideTable();
+        toggleButton.innerText = 'Показать таблицу Пифагора';
+    }
+}
+
+document.getElementById('toggleTableButton').addEventListener('click', toggleTable);
